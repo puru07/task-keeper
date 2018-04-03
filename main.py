18b07c3c60e220f9d469ee8a9cdf330ce239cccd
puru07 <pururastogi@gmail.com>
2018-04-02 22:14:36 -0400

    add month class and some more stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 # @brief this module contains various Important Structure
 # @author: Puru Rastogi
@@ -21,17 +21,6 @@ class task():
         self.discrip = discrip
         self.date = tasktime
 
-class daytask(record):
-    def __init__(self):
-        self.daytime = datetime.now()
-    def addtask(self,newtask):
-        self.data[newtask.title] = newtask
-    def show(self):
-        # @todo: formatting of tasks printing
-        
-    def add(self,title,discrip='',tasktime = datetime.now()):
-        self.addtask(task(title,discrip,tasktime))
-
 class record():
     def __init__(self):
         self.data = {}
@@ -47,6 +36,57 @@ class record():
             self.data[key].append(value)
         else:
             self.data[key] = [value]
+
+    def show(self):
+        # @todo: formatting of tasks printing
+        od = sorted(self.data)
+        for item in od:
+            print(item[0])
+            print("-------")
+            for rdata in item[1] :
+                if rdata is str:
+                    print(rdata)
+                else rdata.show()
+
+class daytask(record):
+    def __init__(self):
+        self.instime = datetime.now()
+    def addtask(self,newtask):
+        self.data[newtask.title] = newtask.discrip
+
+    def add(self,title,discrip='',tasktime = datetime.now()):
+        self.addtask(task(title,discrip,tasktime))
+
+class month():
+    def __init__():
+        self.instime = datetime.now()
+        self.monthtask = record()
+        self.perdaytask = record()
+    
+    def add(self,newtask):
+        if type(newtask) is daytask:
+            self.adddaytask(newtask)
+        else:
+            self.addtask(newtask)
+    
+    def addtask(self, newtask):
+        'task for the whole month'
+        self.monthtask[newtask.title] = newtask
+    
+    def adddaytask(self, daytask):
+        'task for each day'
+        self.perdaytask[daytask.instime.day] = daytask
+
+    def show(self):
+        # whole month tasks
+        self.monthtask.show()
+
+
+
+
+
+
+
 
 # class almanac():
 #     def __init__(self):

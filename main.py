9bdfce6af9439c7b70dc2e0ca5f8c93f6a7f1647
puru07 <pururastogi@gmail.com>
2018-05-16 01:11:32 -0400

    make it load and save the almanac

--- main.py
+++ main.py
@@ -12,33 +12,52 @@ from datetime import datetime
 from taskmodule import *
 import pickle
 
-def setup():
-    dir_name = 'taskkeeper'
-    if not os.path.isdir(os.exapnduser('~/'+ dir_name)):
-        os.path.mkdir(os.expanduser('~/' + dir_name))
-    
+
+def setup(name=None):
+    flolder_name = 'taskkeeper'
+    dir_name = os.path.expanduser('~/' + flolder_name)
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+    filename = dir_name + '/almanac.pkl'
+    if name is not None:
+        filename = dir_name + '/' + name + '.pkl'
+    return datamng(filename)
 
 
 class datamng():
-    def __init__(self, name=None):
-        self.name = 'alamanac.pkl'
-        if not name is None:
-            self.name = name + '.pkl'
-    def save(self, data):
+    def __init__(self, name):
+        self.name = name
+
+        # try to restore old data
+        alm = self.restore()
+
+        # if no data is found
+        if alm is None:
+            alm = almanac()
+        self.almanac = alm
+
+    def save(self):
+        'save the current almanac'
         with open(self.name, 'wb') as dataobj:
-            pickle.dump(data, dataobj)
+            pickle.dump(self.almanac, dataobj)
 
     def restore(self):
+        'restore any previosly saved almanac'
         if not os.path.isfile(self.name):
             return None
-        with open(self.name,'rb') as dataobj:
+        with open(self.name, 'rb') as dataobj:
             return pickle.load(dataobj)
 
-def main():
-    dmanage = datamng()
-    alm = alamanac()
-    dmanage.save(p)
 
-if __name__=='__main__':
+def main():
+    datamng = setup()
+    almanac = datamng.almanac
+    # almanac.add('uasc', 'do this and that')
+    # almanac.add('hb', 'do this')
+    almanac.show()
+    datamng.save()
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='task_keeper',usage='%(prog)s action [data]')
-    
+    main()
--- taskmodule.py
+++ taskmodule.py
@@ -9,8 +9,9 @@ import time
 import sys
 from datetime import datetime
 
+
 class task():
-    def __init__(self,title ,discrip='',tasktime = None):
+    def __init__(self, title, discrip='', tasktime=None):
         self.title = title.lower()
         self.discrip = discrip
         if tasktime is None:
@@ -22,16 +23,19 @@ class task():
 
 
 class record():
+    '''
+    @description: basic data type for recoding any task
+    '''
     def __init__(self):
         self.data = {}
-    
-    def __getitem__(self,key):
+
+    def __getitem__(self, key):
         if key in self.data:
             return self.data[key]
-        else :
+        else:
             return []
 
-    def __setitem__(self,key,value):
+    def __setitem__(self, key, value):
         if key in self.data:
             self.data[key].append(value)
         else:
@@ -39,31 +43,33 @@ class record():
 
     def show(self):
         # @todo: formatting of tasks printing
-        od = sorted(self.data.items())
+        # od = sorted(self.data.items())
+        od = list(self.data.items())
         for item in od:
-            print(item[0])
-            print("-------")
-            for rdata in item[1] :
-                if rdata is str:
+            if type(item[0]) is str:
+                print(item[0])
+            for rdata in item[1]:
+                if type(rdata) is str:
                     print(rdata)
                 else:
                     rdata.show()
 
+
 class daytask():
-    def __init__(self,taskortitle=None, discrip='',tasktime=None):
+    def __init__(self, taskortitle=None, discrip='', tasktime=None):
         self.instime = datetime.now()
-        self.taskrecord =record()
-        if not taskortitle is None:
-            self.add(taskortitle, discrip,tasktime)
+        self.taskrecord = record()
+        if taskortitle is not None:
+            self.add(taskortitle, discrip, tasktime)
 
-    def add(self,taskortitle, discrip='',tasktime=None):
+    def add(self, taskortitle, discrip='', tasktime=None):
         'generic add method'
-        self.addtask(taskortitle, discrip,tasktime)
+        self.addtask(taskortitle, discrip, tasktime)
 
-    def addtask(self,newtask, discrip='',tasktime=None):
+    def addtask(self, newtask, discrip='', tasktime=None):
         'add task obj'
         if type(newtask) is str:
-            newtask = task(newtask,discrip,tasktime)
+            newtask = task(newtask, discrip, tasktime)
         self.taskrecord[newtask.title] = newtask
 
     def show(self):
@@ -71,20 +77,20 @@ class daytask():
 
 
 class monthtask(daytask):
-    def __init__(self,taskortitle=None, discrip='',tasktime=None):
+    def __init__(self, taskortitle=None, discrip='', tasktime=None):
         self.instime = datetime.now()
         self.taskrecord = record()   # single task for complete month
         self.perdaytask = record()  # tasks for each day
-        
-        if not taskortitle is None:
-            self.add(taskortitle, discrip,tasktime)
 
-    def add(self,taskortitle, discrip='',tasktime=None):
+        if taskortitle is not None:
+            self.add(taskortitle, discrip, tasktime)
+
+    def add(self, taskortitle, discrip='', tasktime=None):
         'generic add method'
         if type(taskortitle) is daytask:
             self.adddaytask(taskortitle)
-        else :
-            self.addtask(taskortitle, discrip,tasktime)
+        else:
+            self.addtask(taskortitle, discrip, tasktime)
 
     def adddaytask(self, newtask):
         'task for each day'
@@ -97,24 +103,26 @@ class monthtask(daytask):
         # perday tasks
         self.perdaytask.show()
 
-class  yeartask(monthtask):
-    def __init__(self,taskortitle=None, discrip='',tasktime=None):
+
+class yeartask(monthtask):
+    def __init__(self, taskortitle=None, discrip='', tasktime=None):
         self.instime = datetime.now()
         self.taskrecord = record()      # single task for complete month
         self.permonthtask = record()    # tasks for each month
-        
-        if not taskortitle is None:
-            self.add(taskortitle, discrip,tasktime)
 
-    def add(self,taskortitle, discrip='',tasktime=None):
+        if taskortitle is not None:
+            self.add(taskortitle, discrip, tasktime)
+
+    def add(self, taskortitle, discrip='', tasktime=None):
         'generic add month'
         if type(taskortitle) is monthtask:
             self.addmonthtask(taskortitle)
         else:
-            self.addtask(taskortitle, discrip,tasktime)
-    def addmonthtask(self,newtask):
+            self.addtask(taskortitle, discrip, tasktime)
+
+    def addmonthtask(self, newtask):
         self.permonthtask[newtask.instime.month] = newtask
-    
+
     def show(self):
         # whole year tasks
         self.taskrecord.show()
@@ -122,15 +130,18 @@ class  yeartask(monthtask):
         # per month tasks
         self.permonthtask.show()
 
-class almanac():
-    'the diary'
-    def __init__(self,taskortitle=None, discrip='',tasktime=None):
-        self.records = record()
-        if not taskortitle is None:
-            self.add(taskortitle)
 
-    def add(self,newtask, discrip='',tasktime=None):
-        self.records[task(yeartask(monthtask(daytask(newtask, discrip='',tasktime=None))))] = newtask
+class almanac():
+    'the complete diary'
+    def __init__(self, taskortitle=None, discrip='', tasktime=None):
+        self.records = record()
+        if taskortitle is not None:
+            self.add(taskortitle, discrip, tasktime)
+
+    def add(self, newtask, discrip='', tasktime=None):
+        'add a task to almanac'
+        year_task = yeartask(monthtask(daytask(newtask, discrip, tasktime)))
+        self.records[year_task] = year_task
 
     def show(self):
         self.records.show()
